fffd14c2f1d90c3395f5682aeace8092092c36e7
Trent-Duncan <duncantr131@northwested.org>
2024-09-16 14:05:10 -0400

    Add files via upload

---
+++ mad_lib.py
@@ -0,0 +1,21 @@
+# Trent Duncan
+# 9/16/24
+# Madlib
+
+
+
+noun = input("Noun")
+noun2 = input("Noun")
+noun3 = input("Noun")
+adjitive = input("Adjitive")
+noun4 = input("Noun")
+verb = input("Verb")
+
+
+
+
+madlib= f"hey this is my {noun} i hope you grabbed your {noun2} and some yummy {noun3} becuase were going to be {adjitive} today so get ready for a fun {noun4} this will be {verb}."
+
+
+
+print(madlib)
